8e644c55bc92b98a1288fe88a903e0e6bcc8be16
Juan Carlos Camacho ruiz <juan.camacho@despegar.com>
2020-05-24 22:33:23 -0300

    hellow world aspects

---
+++ src/main/java/com/example/app/controller/EmployeeController.java
@@ -0,0 +1,28 @@
+package com.example.app.controller;
+
+import com.example.app.model.Employee;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import com.example.app.service.EmployeeService;
+
+@RestController
+public class EmployeeController {
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @RequestMapping(value = "/add/employee", method = RequestMethod.GET)
+    public Employee addEmployee(@RequestParam("name") String name, @RequestParam("empId") String empId) {
+        return employeeService.createEmployee(name, empId);
+    }
+
+    @RequestMapping(value = "/remove/employee", method = RequestMethod.GET)
+    public String removeEmployee( @RequestParam("empId") String empId) {
+        employeeService.deleteEmployee(empId);
+        return "Employee removed";
+    }
+}
